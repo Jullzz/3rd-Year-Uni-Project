c5ace59b6347bbd46e12c8e0a9bbf04a9ea53cc3
James Schifferle <jschiff98@gmail.com>
2019-04-05 13:18:17 +1100

    Initial commit

---
+++ components/Map.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div id="map" class="w-full h-full bg-red z-0" :class="(!enlarged) ? 'map-short': 'map-tall'" ></div>
+    <div id="circle" class="shadow-lg -mt-10 bg-blue" @click="enlarged = !enlarged" >
+      <svg v-if="!enlarged" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-cheveron-down"><path class="secondary" fill-rule="evenodd" d="M15.3 10.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/></svg>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-cheveron-up"><path class="secondary" fill-rule="evenodd" d="M8.7 13.7a1 1 0 1 1-1.4-1.4l4-4a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1-1.4 1.4L12 10.42l-3.3 3.3z"/></svg>
+    </div>
+  </div>
+</template>
+
+<script>
+import GoogleMapsLoader from 'google-maps'
+
+export default {
+  data() {
+    return {
+      enlarged: false
+    }
+  },
+  mounted: () => {
+    console.log("init map")
+    GoogleMapsLoader.KEY = 'AIzaSyBVAaFiYCWzkMHq2O9HNYAfeGpo6u8ilKQ'
+    GoogleMapsLoader.load(function(google) {
+      new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lat: -36.757042, lng: 144.279056}
+      });
+    });
+  }
+}
+</script>
+
+<style>
+.map-short {
+  height: 250px;
+  transition: height 0.2s;
+}
+
+.map-tall {
+  transition: height 0.2s;
+  height: 600px;
+}
+
+#circle {
+  position:absolute;
+  z-index: 100;
+  width: 80px;
+  height: 80px;
+  -webkit-border-radius: 40px;
+  -moz-border-radius: 40px;
+  border-radius: 40px;
+  left: 50%;
+  margin-left: -40px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <Map class="w-full h-full" ></Map>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Map from '~/components/Map.vue'
+
+export default {
+  components: {
+    Logo,
+    Map
+  }
+}
+</script>
