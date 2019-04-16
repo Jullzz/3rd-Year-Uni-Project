8e85c4efd23ebcbe222764c4609c7ae5ba74eebf
James Schifferle <jschiff98@gmail.com>
2019-04-16 17:08:39 +1000

    initial commit

---
+++ api/server.js
@@ -0,0 +1,19 @@
+var express = require("express")
+var app = express()
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+// Root path
+app.get("/", (req, res, next) => {
+    res.json({"data": "hello"})
+});
+
