7ad4706d18cd93e295fdca4f57089813acfe2bc0
unknown <jvern1998@gmail.com>
2019-09-11 14:39:08 +1000

    test11

--- flask/app.py
+++ flask/app.py
@@ -26,7 +26,7 @@ def get_data():
 
 @app.route(URL_BASE + 'test', methods=['POST'])
 def test():
-    print(request.get_data().app_id, file=sys.stderr)
+    print(request.get_data(), file=sys.stderr)
     print("done", file=sys.stderr)
     return "done"
 
