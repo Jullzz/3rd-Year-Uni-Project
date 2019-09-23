08ea769c80e266193ad664b2cac1500f67bb4d37
JSchifff <jschiff98@gmail.com>
2019-09-23 13:17:19 +1000

    Fixed bad address

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -4,7 +4,7 @@ const pkg = require('./package')
 module.exports = {
   server: {
     port: 3000, // default: 3000
-    host: 'localhost', // default: localhost
+    host: '0.0.0.0', // default: localhost
   },
   mode: 'spa',
 
