c69cad3d4c70571f4f8a6a9105581a3d5a8de175
Julian <jvern1998@gmail.com>
2019-09-29 00:23:18 +1000

    .

--- api/server.js
+++ api/server.js
@@ -132,6 +132,6 @@ app.get(BASE_URL + "frontPull", (req, res, next)=>{
     let timestamp = ((date/1000)-(date%1000/1000));
     let s = new Date(timestamp).toISOString();
     console.log(s);
-    influx.query('SELECT * FROM cpu_load_short WHERE time > ' + s).then(data =>
+    influx.query('SELECT * FROM cpu_load_short WHERE time > "' + s + '"').then(data =>
     res.json(data)).catch(err=> res.status(404).json({error: err.message}));;
 });
