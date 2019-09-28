b948aebb62bc87f88b852cb8da6c144c44f9f739
Julian <jvern1998@gmail.com>
2019-09-28 15:19:54 +1000

    .

--- api/server.js
+++ api/server.js
@@ -101,7 +101,7 @@ app.get(BASE_URL + "test/populatedb", (req, res, next) => {
 
 app.get(BASE_URL + "test/singleData", (req, res, next) => {
     console.log("SinglePoint Reached");
-    console.log(res);
+    console.log(res.body[0].title);
     /*
     influx.writePoints([
         {
