a49d8641078bdc17d90facf0164f54c2da524265
Julian <jvern1998@gmail.com>
2019-09-28 22:10:56 +1000

    .

--- api/server.js
+++ api/server.js
@@ -126,9 +126,8 @@ app.get(BASE_URL + "sendSingleData", (req, res, next) => {
 });
 
 app.get(BASE_URL + "frontPull", (req, res, next)=>{
-    let data = JSON.parse(req.body);
     let date = new Date();
     let timestamp = ((date/1000)-(date%1000/1000));
-    influx.query('SELECT * FROM "cpu_load_short"').then(data =>
+    influx.query('SELECT * FROM cpu_load_short').then(data =>
     res.json(data)).catch(err=> res.status(404).json({error: err.message})).then(console.log("POOP"));;;
 });
