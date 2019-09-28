c3f8d7879fecee79d85f418b7c6c1700c750257b
Julian <jvern1998@gmail.com>
2019-09-28 20:07:40 +1000

    .

--- api/server.js
+++ api/server.js
@@ -97,6 +97,7 @@ app.get(BASE_URL + "test/writePoint", (req, res, next) => {
 
 app.get(BASE_URL + "sendSingleData", (req, res, next) => {
     let data = JSON.parse(req.body);
+    console.log(data.timestamp);
     influx.writePoints([
         {
             measurement: 'cpu_load_short',
